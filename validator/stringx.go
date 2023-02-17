e8968f9f4b4ff47dfa45176292b46ed9c2ecfc30
lizhuyou <wxlizhuyou@gf.com.cn>
2023-02-17 18:00:28 +0800

    优化

--- validator/stringx.go
+++ validator/stringx.go
@@ -1,10 +1,6 @@
 package validator
 
 import (
-	"encoding/json"
-	"fmt"
-	"gitlab.gf.com.cn/hk-common/go-tool/server/logger"
-	"strconv"
 	"strings"
 )
 
@@ -14,42 +10,6 @@ const (
 )
 
 type TempParam map[string]string
-
-func Template(s string, param TempParam) string {
-	for k, v := range param {
-		s = strings.ReplaceAll(s, fmt.Sprintf("{{%s}}", k), v)
-	}
-	return s
-}
-
-func ParseInt64(s string) int64 {
-	n, _ := strconv.ParseInt(s, 10, 64)
-	return n
-}
-
-func ParseFloat(s string) float64 {
-	n, _ := strconv.ParseFloat(s, 64)
-	return n
-}
-
-func Atoi(s string) int {
-	n, _ := strconv.Atoi(s)
-	return n
-}
-
-func Judge(b bool, v1, v2 string) string {
-	if b {
-		return v1
-	}
-	return v2
-}
-
-func ToStrings(s, split string) []string {
-	if s == "" {
-		return []string{}
-	}
-	return strings.Split(s, split)
-}
 
 func Snake(s string) string {
 	data := make([]byte, 0, len(s)*2)
@@ -70,40 +30,4 @@ func Snake(s string) string {
 	}
 	//ToLower把大写字母统一转小写
 	return strings.ToLower(string(data[:]))
-}
-
-func Includes(src []string, dist string) bool {
-	for _, s := range src {
-		if s == dist {
-			return true
-		}
-	}
-	return false
-}
-
-func Delete(src []string, dist string) []string {
-	j := 0
-	for _, v := range src {
-		if v != dist {
-			src[j] = v
-			j++
-		}
-	}
-	return src[:j]
-}
-
-func Marshal(src interface{}) string {
-	bt, err := json.Marshal(src)
-	if err != nil {
-		logger.Error(err)
-	}
-	return string(bt)
-}
-
-func ToArray1(src string) []string {
-	return strings.Split(src, COMMA)
-}
-
-func ToArray2(src string) []string {
-	return strings.Split(src, SEMICOLON)
 }
